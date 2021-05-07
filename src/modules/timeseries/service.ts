418038f67ed5b4d1aca17394b980140249f1db0e
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-05-07 07:17:28 +0530

    feat(timeseries): return empty array if key does not exist

--- src/modules/timeseries/service.ts
+++ src/modules/timeseries/service.ts
@@ -3,8 +3,12 @@ import { TimeRow } from './interface'
 
 class TimeSeriesService {
   static async getAllById(machineID: string): Promise<Array<TimeRow>> {
-    const details = await timeSeriesClient.range(machineID, '0', `${Date.now()}`)
-    return (details as unknown) as Promise<Array<TimeRow>>
+    try {
+      const details = await timeSeriesClient.range(machineID, '0', `${Date.now()}`)
+      return (details as unknown) as Promise<Array<TimeRow>>
+    } catch (e) {
+      return []
+    }
   }
 }
 
